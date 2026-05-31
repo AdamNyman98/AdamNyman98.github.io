a4389fe0335b9e234235fdd27d12181b30a615b3
Lya <adam98nyman@gmail.com>
2026-05-31 22:00:34 +0200

    worlds collide cards should now work with touch and should not be outside of 0

--- WorldsCollide/index.js
+++ WorldsCollide/index.js
@@ -3,6 +3,7 @@ let newX = 0, newY = 0, startX = 0, startY = 0;
 const card = document.getElementById('card')
 
 card.addEventListener('mousedown', mouseDown)
+card.addEventListener('touchstart', touchStart)
 
 function mouseDown(e){
     startX = e.clientX;
@@ -19,10 +20,33 @@ function mouseMove(e){
     startX = e.clientX;
     startY = e.clientY;
 
-    card.style.top = (card.offsetTop - newY) + 'px';
-    card.style.left = (card.offsetLeft - newX) + 'px';
+    card.style.top = (card.offsetTop - newY) >= 0 ? (card.offsetTop - newY)  + 'px' : 0 + 'px';
+    card.style.left = (card.offsetLeft - newX) >= 0 ? (card.offsetLeft - newX) + 'px' : 0 + 'px';
 }
 
 function mouseUp(e){
     document.removeEventListener('mousemove', mouseMove)
+}
+
+function touchStart(e){
+    startX = e.clientX;
+    startY = e.clientY;
+
+    document.addEventListener('touchMove', touchMove)
+    document.addEventListener('touchend', touchEnd)
+}
+
+function touchMove(e){
+    newX = startX - e.touches[0].clientX;
+    newY = startY - e.touches[0].clientY;
+
+    startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
+
+    card.style.top = (card.offsetTop - newY) >= 0 ? (card.offsetTop - newY)  + 'px' : 0 + 'px';
+    card.style.left = (card.offsetLeft - newX) >= 0 ? (card.offsetLeft - newX) + 'px' : 0 + 'px';
+}
+
+function touchEnd(e){
+    document.removeEventListener('touchmove', touchMove)
 }
